ffbbd054edd2a9e005eb92d888c98d0c174e8de5
kshitizbharti <kshitiz.bharti@gmail.com>
2026-01-30 14:45:54 +0530

    Update notifier.py

--- notifier.py
+++ notifier.py
@@ -1,13 +1,16 @@
 import requests
 
-def send(bot_token, chat_id, message):
-    url = f"https://api.telegram.org/bot{bot_token}/sendMessage"
-    requests.post(
-        url,
-        json={
-            "chat_id": chat_id,
-            "text": message,
-            "parse_mode": "HTML"
-        },
-        timeout=10
-    )
+def send(token, chat_id, message):
+    url = f"https://api.telegram.org/bot{token}/sendMessage"
+    payload = {
+        "chat_id": chat_id,
+        "text": message,
+        "parse_mode": "HTML",
+        "disable_web_page_preview": True
+    }
+
+    r = requests.post(url, json=payload, timeout=15)
+    if r.status_code != 200:
+        print("❌ Telegram error:", r.status_code, r.text)
+    else:
+        print("✅ Telegram sent")
