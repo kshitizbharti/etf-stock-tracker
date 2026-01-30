8dcd3f8945aefd07ef30d452551ed4f162cdd782
kshitizbharti <kshitiz.bharti@gmail.com>
2026-01-30 14:51:39 +0530

    Update etf_symbols.py

--- etf_symbols.py
+++ etf_symbols.py
@@ -1,5 +1,58 @@
+# NSE ETFs – Yahoo Finance compatible (.NS)
+# Curated for maximum coverage + reliability
+
 ETF_SYMBOLS = [
-    "NIFTYBEES.NS","BANKBEES.NS","ITBEES.NS","PHARMABEES.NS",
-    "GOLDBEES.NS","SILVERBEES.NS","CPSEETF.NS","PSUBNKBEES.NS",
-    "MON100.NS","MID150BEES.NS","SBIETFQLTY.NS"
+
+    # Broad Market
+    "NIFTYBEES.NS",
+    "SETFNIF50.NS",
+    "ICICINIFTY.NS",
+    "HDFCNIFTY.NS",
+    "UTINIFTY.NS",
+    "SBINIFTY.NS",
+
+    # Bank / Financial
+    "BANKBEES.NS",
+    "SETFNIFBK.NS",
+    "ICICIBANKETF.NS",
+    "SBIETFBANK.NS",
+    "PSUBNKBEES.NS",
+
+    # Mid / Small
+    "MID150BEES.NS",
+    "SBIETFMID.NS",
+    "SBIETFSML.NS",
+
+    # Sectoral
+    "ITBEES.NS",
+    "PHARMABEES.NS",
+    "AUTOBEES.NS",
+    "FMCGBEES.NS",
+    "ENERGYBEES.NS",
+    "METALBEES.NS",
+
+    # Smart Beta
+    "SBIETFQLTY.NS",
+    "SBIETFMOM.NS",
+    "SBIETFALPH.NS",
+    "SBIETFCON.NS",
+
+    # Gold
+    "GOLDBEES.NS",
+    "HDFCGOLD.NS",
+    "KOTAKGOLD.NS",
+    "AXISGOLD.NS",
+    "SBIGOLD.NS",
+    "ICICIGOLD.NS",
+
+    # Silver
+    "SILVERBEES.NS",
+    "SILVERETF.NS",
+
+    # CPSE / PSU
+    "CPSEETF.NS",
+
+    # International
+    "MON100.NS",
+    "MOSESETF.NS",
 ]
